eb23f35bf1b8ffc546af3a5ba0e754e691531d9f
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2021-10-14 16:45:22 +0300

    test

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 const navMenu = document.getElementById('nav-menu'),
       showMenu = document.getElementById('btn-menu'),
       closeMenu = document.getElementById('btn-close'),
-      sectionsHidden = document.querySelectorAll('.section, .background-abstraction');
+      sectionsHidden = document.querySelectorAll('.move, .background-abstraction');
 
 if(showMenu){
    showMenu.addEventListener('click', () =>{
       navMenu.classList.add('show-menu');
       showMenu.classList.remove('show-menu-btn');
       sectionsHidden.forEach((item => 
-         item.classList.add('section-hidden')
+         item.classList.add('move-hidden')
       ));
    })
 }
@@ -18,7 +18,7 @@ if(closeMenu){
       navMenu.classList.remove('show-menu');
       showMenu.classList.add('show-menu-btn');
       sectionsHidden.forEach((item => 
-         item.classList.remove('section-hidden')
+         item.classList.remove('move-hidden')
       ));
    })
 }
