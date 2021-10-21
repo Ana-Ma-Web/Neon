e869d17f8f28daedbea3a2ec8d6ddec158e2d8da
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2021-10-21 22:43:16 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 // ===== SHOW MENU =====
 const navMenu = document.getElementById('nav-menu'),
-      showMenu = document.getElementById('btn-menu'),
-      closeMenu = document.getElementById('btn-close'),
-      sectionsHidden = document.querySelectorAll('.move, .background-abstraction'),
-      hueColor = document.getElementById('hue-color'),
-      body = document.querySelector('body');
+			showMenu = document.getElementById('btn-menu'),
+			closeMenu = document.getElementById('btn-close'),
+			sectionsHidden = document.querySelectorAll('.move, .background-abstraction'),
+			hueColor = document.getElementById('hue-color'),
+			body = document.querySelector('body');
 
 if(showMenu){
-   showMenu.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu');
-      showMenu.classList.remove('show-menu-btn');
-      sectionsHidden.forEach((item => 
-         item.classList.add('move-hidden')
-      ));
-   })
+	showMenu.addEventListener('click', () =>{
+			navMenu.classList.add('show-menu');
+			showMenu.classList.remove('show-menu-btn');
+			sectionsHidden.forEach((item => 
+				item.classList.add('move-hidden')
+			));
+	})
 }
 if(closeMenu){
-   closeMenu.addEventListener('click', () => {
-      navMenu.classList.remove('show-menu');
-      showMenu.classList.add('show-menu-btn');
-      sectionsHidden.forEach((item => 
-         item.classList.remove('move-hidden')
-      ));
-   })
+	closeMenu.addEventListener('click', () => {
+			navMenu.classList.remove('show-menu');
+			showMenu.classList.add('show-menu-btn');
+			sectionsHidden.forEach((item => 
+				item.classList.remove('move-hidden')
+			));
+	})
 }
 
+const swiper = new Swiper('.swiper', {
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1.1,
+});
 
